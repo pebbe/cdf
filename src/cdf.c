90e893c61cd93cdffc99f46e9c3bbc5a98b31009
Peter Kleiweg <kleiweg@ziggo.nl>
2025-09-20 13:27:13 +0200

    bugfix

--- src/cdf.c
+++ src/cdf.c
@@ -10,26 +10,21 @@
 
 #include <netcdf.h>
 
-#define XXnc(cmd)                         \
-    {                                     \
-        int status = (cmd);               \
-        if (status)                       \
-        {                                 \
-            fprintf(stderr,               \
-                    "%s:%i: error: %s\n", \
-                    __FILE__,             \
-                    __LINE__,             \
-                    nc_strerror(status)); \
-            return 1;                     \
-        }                                 \
+#define XXnc(cmd)                                                              \
+    {                                                                          \
+        int status = (cmd);                                                    \
+        if (status) {                                                          \
+            fprintf(stderr, "%s:%i: error: %s\n", __FILE__, __LINE__,          \
+                    nc_strerror(status));                                      \
+            return 1;                                                          \
+        }                                                                      \
     }
 
 int doDouble(int ncid, int varid, int ndims);
 int doString(int ncid, int varid, int ndims);
 char *quote(char *s);
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
     assert(argc == 2);
 
     int ncid;
@@ -40,15 +35,14 @@ int main(int argc, char **argv)
 
     char dimnames[ndims][NC_MAX_NAME + 1];
     size_t dimlens[ndims];
-    for (int i = 0; i < ndims; i++)
-    {
+    for (int i = 0; i < ndims; i++) {
         XXnc(nc_inq_dimname(ncid, i, dimnames[i]));
         XXnc(nc_inq_dimlen(ncid, i, &dimlens[i]));
     }
     printf("{\n  \"dimensions\": {\n");
-    for (int i = 0; i < ndims; i++)
-    {
-        printf("    \"%s\": %lu%s\n", quote(dimnames[i]), dimlens[i], i == ndims - 1 ? "" : ",");
+    for (int i = 0; i < ndims; i++) {
+        printf("    \"%s\": %lu%s\n", quote(dimnames[i]), dimlens[i],
+               i == ndims - 1 ? "" : ",");
     }
     printf("  },\n  \"variables\": {");
 
@@ -57,8 +51,7 @@ int main(int argc, char **argv)
         if (nc_inq_varname(ncid, nvars, NULL))
             break;
 
-    for (int varid = 0; varid < nvars; varid++)
-    {
+    for (int varid = 0; varid < nvars; varid++) {
         char name[NC_MAX_NAME + 1];
         nc_type xtype;
         int ndims, natts;
@@ -66,8 +59,7 @@ int main(int argc, char **argv)
 
         printf("%s\n    \"%s\": {", varid ? "," : "", quote(name));
 
-        for (int attnum = 0;; attnum++)
-        {
+        for (int attnum = 0;; attnum++) {
             char name[NC_MAX_NAME + 1];
             if (nc_inq_attname(ncid, varid, attnum, name))
                 break;
@@ -75,56 +67,60 @@ int main(int argc, char **argv)
             XXnc(nc_inq_atttype(ncid, varid, name, &xtype));
             printf("%s\n      \"%s\": ", attnum ? "," : "", quote(name));
 
-            if (xtype == NC_CHAR)
-            {
+            if (xtype == NC_CHAR) {
                 size_t len;
                 XXnc(nc_inq_attlen(ncid, varid, name, &len));
                 char v[len + 1];
                 v[len] = '\0';
                 XXnc(nc_get_att_text(ncid, varid, name, v));
                 printf("\"%s\"", quote(v));
-            }
-            else if (xtype == NC_DOUBLE)
-            {
+
+            } else if (xtype == NC_STRING) {
+                size_t len;
+                XXnc(nc_inq_attlen(ncid, varid, name, &len));
+                char *v[len];
+                memset(v, 0, len * sizeof(char *));
+                XXnc(nc_get_att_string(ncid, varid, name, v));
+                if (len == 1)
+                    printf("\"%s\"", quote(v[0]));
+                else {
+                    printf("null");
+                }
+                XXnc(nc_free_string(len, v));
+            } else if (xtype == NC_DOUBLE) {
                 double v;
                 XXnc(nc_get_att_double(ncid, varid, name, &v));
-                printf("%g", v);
-            }
-            else if (xtype == NC_FLOAT)
-            {
+                if (isnan(v))
+                    printf("-9999");
+                else
+                    printf("%g", v);
+            } else if (xtype == NC_FLOAT) {
                 float v;
                 XXnc(nc_get_att_float(ncid, varid, name, &v));
-                printf("%g", v);
-            }
-            else
-            {
+                if (isnan(v))
+                    printf("-9999");
+                else
+                    printf("%g", v);
+            } else {
                 printf("null");
             }
         }
 
         if (xtype == NC_DOUBLE)
-        {
-            if (doDouble(ncid, varid, ndims))
-                return 1;
-        }
+            doDouble(ncid, varid, ndims);
         else if (xtype == NC_STRING)
-        {
-            if (doString(ncid, varid, ndims))
-                return 1;
-        }
+            doString(ncid, varid, ndims);
 
         printf("\n    }");
     }
     printf("\n  }\n}\n");
 }
 
-int doDouble(int ncid, int varid, int ndims)
-{
+int doDouble(int ncid, int varid, int ndims) {
     if (ndims < 1 || ndims > 2)
         return 1;
 
-    if (ndims == 1)
-    {
+    if (ndims == 1) {
         int dimids[1];
         XXnc(nc_inq_vardimid(ncid, varid, dimids));
 
@@ -135,13 +131,16 @@ int doDouble(int ncid, int varid, int ndims)
         XXnc(nc_get_var_double(ncid, varid, v));
 
         printf(",\n      \"values\": [");
-        for (int i = 0; i < dimlen; i++)
-        {
-            long int li = v[i];
-            if (v[i] - li)
-                printf("%s\n        %g", i ? "," : "", v[i]);
-            else
-                printf("%s\n        %li", i ? "," : "", li);
+        for (int i = 0; i < dimlen; i++) {
+            if (isnan(v[i]))
+                printf("%s\n        -9999", i ? "," : "");
+            else {
+                long int li = v[i];
+                if (v[i] - li)
+                    printf("%s\n        %g", i ? "," : "", v[i]);
+                else
+                    printf("%s\n        %li", i ? "," : "", li);
+            }
         }
         printf("\n      ]");
 
@@ -159,17 +158,19 @@ int doDouble(int ncid, int varid, int ndims)
     XXnc(nc_get_var_double(ncid, varid, &(v[0][0])));
 
     printf(",\n      \"values\": [");
-    for (int j = 0; j < dimlen[1]; j++)
-    {
-        if (dimlen[1] > 1) 
+    for (int j = 0; j < dimlen[1]; j++) {
+        if (dimlen[1] > 1)
             printf("%s\n        [", j ? "," : "");
-        for (int i = 0; i < dimlen[0]; i++)
-        {
-            long int li = v[i][j];
-            if (v[i][j] - li)
-                printf("%s\n          %g", i ? "," : "", v[i][j]);
-            else
-                printf("%s\n          %li", i ? "," : "", li);
+        for (int i = 0; i < dimlen[0]; i++) {
+            if (isnan(v[i][j]))
+                printf("%s\n        -9999", i ? "," : "");
+            else {
+                long int li = v[i][j];
+                if (v[i][j] - li)
+                    printf("%s\n          %g", i ? "," : "", v[i][j]);
+                else
+                    printf("%s\n          %li", i ? "," : "", li);
+            }
         }
         if (dimlen[1] > 1)
             printf("\n        ]");
@@ -179,8 +180,7 @@ int doDouble(int ncid, int varid, int ndims)
     return 0;
 }
 
-int doString(int ncid, int varid, int ndims)
-{
+int doString(int ncid, int varid, int ndims) {
     if (ndims != 1)
         return 1;
 
@@ -194,23 +194,19 @@ int doString(int ncid, int varid, int ndims)
     XXnc(nc_get_var_string(ncid, varid, v));
 
     printf(",\n      \"values\": [");
-    for (int i = 0; i < dimlen; i++)
-    {
+    for (int i = 0; i < dimlen; i++) {
         printf("%s\n        \"%s\"", i ? "," : "", quote(v[i]));
     }
     printf("\n      ]");
     return 0;
 }
 
-char *quote(char *s)
-{
+char *quote(char *s) {
     char *s2 = (char *)malloc(1 + 2 * strlen(s));
     assert(s2);
     int j = 0;
-    for (int i = 0; s[i]; i++)
-    {
-        if (s[i] == '\n')
-        {
+    for (int i = 0; s[i]; i++) {
+        if (s[i] == '\n') {
             s[j++] = '\\';
             s[j++] = 'n';
             continue;
